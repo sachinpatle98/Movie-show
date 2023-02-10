b4f1a820c5b5689dac7bfd6c1684ef134ea24fc2
iamsachin <104750845+sachinpatle98@users.noreply.github.com>
2023-02-10 10:15:38 +0530

    Updates

---
+++ precache-manifest.e0c172721f0740c1f9f04b1619f551a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "afb96d5dd7c52c7b2f5b",
+    "url": "/OMDB-Media/static/css/main.b46fb416.chunk.css"
+  },
+  {
+    "revision": "afb96d5dd7c52c7b2f5b",
+    "url": "/OMDB-Media/static/js/main.afb96d5d.chunk.js"
+  },
+  {
+    "revision": "5202d86b1aa418f0e2e1",
+    "url": "/OMDB-Media/static/js/1.5202d86b.chunk.js"
+  },
+  {
+    "revision": "b40bc9486b4fe98cd189",
+    "url": "/OMDB-Media/static/js/runtime~main.b40bc948.js"
+  },
+  {
+    "revision": "da32298a2c2c3a03102b072bb878d9fe",
+    "url": "/OMDB-Media/index.html"
+  }
+];
